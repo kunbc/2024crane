321b071ebfeea29a84c7b0f29b55587dbe59958d
kunbc <2724245916@qq.com>
2024-05-01 00:12:19 +0800

    4.20daima

---
+++ 2024crane/Core/Inc/servo.h
@@ -0,0 +1,21 @@
+#ifndef __SERVO_H__
+#define __SERVO_H__
+
+
+#include "main.h"
+
+void steer_init(double Start_Angle);
+void steer(double target_angle,int Servo_num);
+
+#define SERVO_1         1         /* ¶æ»ú½Ó¿ÚÐòºÅ */
+#define SERVO_2         2
+#define SERVO_ALL       3
+
+
+
+
+
+
+
+
+#endif /* __SERVO_H__ */
